a06d31d297c0917d7b19df7725501b3a9d3b09c3
Sanjay <Sanjaychandak95@gmail.com>
2017-11-16 09:16:51 +0530

    docsx change

--- Development/source/ARS/test/com/cg/ars/test/AirlineServiceImplTest.java
+++ Development/source/ARS/test/com/cg/ars/test/AirlineServiceImplTest.java
@@ -1,17 +1,12 @@
 package com.cg.ars.test;
 
-<<<<<<< HEAD
+
 import static org.junit.Assert.*;
-=======
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.stub;
 import static org.mockito.Mockito.verify;
-
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
->>>>>>> 6e507750599e948f91433e979ed0d391e8c1ee7f
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,15 +14,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import com.cg.ars.dao.AirlineDAO;
-<<<<<<< HEAD
 import com.cg.ars.entity.Flight;
-=======
 import com.cg.ars.entity.BookingInformation;
-import com.cg.ars.entity.Flight;
 import com.cg.ars.entity.User;
->>>>>>> 6e507750599e948f91433e979ed0d391e8c1ee7f
 import com.cg.ars.service.AirlineService;
 import com.cg.ars.service.AirlineServiceImpl;
 
@@ -35,26 +25,6 @@ import com.cg.ars.service.AirlineServiceImpl;
 public class AirlineServiceImplTest {
 	@Mock
 	private AirlineDAO airlineDAO;
-<<<<<<< HEAD
-	
-	@InjectMocks
-	private AirlineService airlineService = new AirlineServiceImpl();
-	
-	@Before
-	public void setUp(){
-		MockitoAnnotations.initMocks(this);
-	}
-
-	/*@Test
-	public void testViewListOfFlights() {
-		Flight flight = new Flight("SG-277","SPICE JET","MAA","CCU",Date.valueOf("2018-01-05"),Date.valueOf("2018-01-05"),"11:50","14:20",30,4567,40,5432,"2 hr 30 min");
-		List<Flight> flights = new ArrayList<Flight>();
-		flights.add(flight);
-		String strTest = flight.getDeptCity()+"="+flight.getArrCity()+"="+flight.getDeptDate();
-		assertEquals(flights,airlineService.viewListOfFlights(strTest, "dest"));
-	}*/
-
-=======
 
 	@InjectMocks
 	private AirlineService airlineService = new AirlineServiceImpl();
@@ -103,5 +73,4 @@ public class AirlineServiceImplTest {
 				airlineService.retrieveBookings(query, searchBasis));
 		verify(airlineDAO).retrieveBookings(query, searchBasis);
 	}
->>>>>>> 6e507750599e948f91433e979ed0d391e8c1ee7f
 }
